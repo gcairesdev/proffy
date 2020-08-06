62afc98a0666f2a591382c12374842e30b254498
gcairesdev <pessoalgcaires@gmail.com>
2020-08-06 12:43:28 -0300

    :sparkles: Added SPA navigation

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.svg';
 import landing from '../../assets/images/landing.svg';
@@ -21,14 +22,14 @@ const Landing = () => {
         <img src={landing} alt='Plataforma de estudos' className='hero-image'/>
 
         <div className='buttons-container'>
-          <a href='' className='give-classes'>
+          <Link to='/give-classes' className='give-classes'>
             <img src={giveClassesIcon} alt='Ícone de dar aula'/>
             Dar aula
-          </a>
-          <a href='' className='study'>
+          </Link>
+          <Link to='/study' className='study'>
             <img src={studyIcon} alt='Ícone de estudo'/>
             Estudar
-          </a>
+          </Link>
         </div>
         <span className='total-connections'>
           Total de 200 conexões já realizadas
