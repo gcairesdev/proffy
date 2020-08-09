e79d547a10aaf5aa22c02c68daf9760b97ec9e75
gcairesdev <pessoalgcaires@gmail.com>
2020-08-09 19:15:37 -0300

    :sparkles: Added search classes function

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -11,6 +11,35 @@ interface ScheduleItem {
 
 export default class ClassesController {
 
+  async index(request: Request, response: Response) {
+    const week_day = request.query.week_day as string;
+    const subject = request.query.subject as string;
+    const time = request.query.time as string;
+
+    if (!week_day || !subject || !time) {
+      return response.status(400).json({
+        error: 'Missing filters to search classes'
+      });
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function() {
+        this.select('classes_schedule.*')
+          .from('classes_schedule')
+          .whereRaw('`classes_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`classes_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`classes_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`classes_schedule`.`to` > ??', [timeInMinutes])
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+    return response.status(200).json(classes);
+  }
+
   async create(request: Request, response: Response) {
     const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
