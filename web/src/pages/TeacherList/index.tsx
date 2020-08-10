a4ed7e2624b21583e5841e8fa7ea93aa6c37c79d
gcairesdev <pessoalgcaires@gmail.com>
2020-08-10 10:29:45 -0300

    :hammer: Used Input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+
+import './styles.css';
 
 const TeacherList = () => {
   return(
     <div id='page-teacher-list' className='container'>
       <PageHeader title='Estes são os proffys disponíveis.' >
         <form id='search-teachers'>
-          <div className='input-block'>
-            <label htmlFor='subject'>Matéria</label>
-            <input type="text" name="subject" id="subject"/>
-          </div>
-
-          <div className='input-block'>
-            <label htmlFor='week-day'>Dia da semana</label>
-            <input type="text" name="week-day" id="week-day"/>
-          </div>
-
-          <div className='input-block'>
-            <label htmlFor='time'>Hora</label>
-            <input type="text" name="time" id="time"/>
-          </div>
+          <Input name='subject' label='Matéria' />
+          <Input name='week_day' label='Dia da semana' />
+          <Input name='time' label='Hora' />
         </form>
       </PageHeader>
 
